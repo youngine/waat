7760968813db459f30010429fb7d770c5c93181d
youngine <youngine012@naver.com>
2022-01-22 01:03:51 +0900

    ETC: gitignore secrets.json

--- config/settings.py
+++ config/settings.py
@@ -1,15 +1,29 @@
 
 from pathlib import Path
+import json,os
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fu!j1-#z+yd%=5u2xe^ky-3qo96gpm0gxj=w7(^f3vv#91hea%'
+
+#secret_file = os.path.join(BASE_DIR, 'secrets.json')
+secret_file = [BASE_DIR /'secrets.json']
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
