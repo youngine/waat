9b7aa0a152df1087bf657e31a267374cd2391582
youngine <youngine012@naver.com>
2022-01-22 12:46:05 +0900

    FEAT: userapp 생성

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
+    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
+    path('user/', include('user.urls')),
 ]
