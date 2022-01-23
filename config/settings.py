bfe72f08918357cc681c2cf72aeb79523bc1a092
CastleRain <jjangsw12@gmail.com>
2022-01-23 14:10:14 +0900

    etc : 병합충돌 제거

--- config/settings.py
+++ config/settings.py
@@ -95,10 +95,7 @@ DATABASES = {
     }
 }
 DATABASE_ROUTERS = ['fundingapp.router.DBRouter']
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/develop
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
