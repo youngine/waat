13cc5cbd063cbdffd18db447229d929e26508994
youngine <youngine012@naver.com>
2022-01-23 16:00:52 +0900

    update: user_login_check

--- user/models.py
+++ user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.fields import CharField
 
-class User_info (models.Model):
+class User_info(models.Model):
     user_id = CharField(max_length=30,primary_key = True)
     user_pw = CharField(max_length=30)
     user_name = CharField(max_length=30)
--- user/views.py
+++ user/views.py
@@ -1,13 +1,26 @@
-from email import message_from_binary_file
-from django.shortcuts import render
-from .models import User_info
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render,redirect
+from .models import  User_info
+from django.http import HttpResponse
 
 def login(request):
-    return render(request, 'user/login.html') 
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            u = User_info.objects.get(user_id=username, user_pw=password)
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+        except User_info.DoesNotExist as e:
+            return HttpResponse('로그인 실패')
+    else:
+        return redirect('/app/main/')
+
 
 def user_list(request):
-    user_list = User_info.objects.all()
+    data = User_info.objects.all()
     return render(
         request, 'user/user_list.html',
-        { 'data': user_list}
+        { 'data': data}
     )
