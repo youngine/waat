e6dd4c862f2e927a26fe27ef0641491668dfed5d
CastleRain <jjangsw12@gmail.com>
2022-01-23 23:23:54 +0900

    Feat : fund 모달 생성

--- fundingapp/views.py
+++ fundingapp/views.py
@@ -2,7 +2,7 @@ import re
 from django.forms import forms
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import FundingBoard, User1, JoinFund, Post
+from .models import FundingBoard, User1, JoinFund, Post, FundingFunc
 from django.views.generic import FormView
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.urls import reverse, reverse_lazy
@@ -45,8 +45,17 @@ def select(request):
 
 def detail(request, board_id):
     data = FundingBoard.objects.filter(board_id=board_id)
-    
+    join_data = FundingFunc.objects.filter(board_id =board_id)
     result = []
+
+    for j in join_data:
+        func_a_expl = j.func_a_expl
+        func_b_expl = j.func_b_expl
+        func_c_expl = j.func_c_expl
+        func_a_price = j.func_a_price
+        func_b_price = j.func_b_price
+        func_c_price = j.func_c_price
+        
 
     for d in data:
         
@@ -57,15 +66,24 @@ def detail(request, board_id):
             "content" : d.content,
             "fund_goal_price" : d.fund_goal_price,
             "fund_total_price" : d.fund_total_price,
-            "percent" : int(d.fund_total_price / d.fund_goal_price * 100)
+             "percent" : int(d.fund_total_price / d.fund_goal_price * 100),
+             "func_a_price" : func_a_price,
+             "func_b_price" : func_b_price,
+             "func_c_price" : func_c_price,
+             "func_a_expl" : func_a_expl,
+             "func_b_expl" : func_b_expl,
+             "func_c_expl" : func_c_expl,
+             
+             
+             
 
         })
-
+    print(result)
     return render(
         request,
         'fund_view/fund_detail.html',
         {
-            "data" : data,
+            "data" : result,
         }
     )
     
