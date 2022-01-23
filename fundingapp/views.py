b6d2edd591ea801059c18ca41f0053da1717fc38
CastleRain <jjangsw12@gmail.com>
2022-01-23 19:00:46 +0900

    fix : fund db 수정

--- fundingapp/views.py
+++ fundingapp/views.py
@@ -4,16 +4,29 @@ from .models import FundingBoard, User1, JoinFund
 def select(request):
     data = FundingBoard.objects.all()
 
+    result = []
 
-    img = []
     for d in data:
-        img.append("/static/" + str(d.board_id) + "jpg")
+        
+        result.append({
+            "board_id" : d.board_id,
+            "user_id" : d.user_id,
+            "title" : d.title,
+            "content" : d.content,
+            "fund_goal_price" : d.fund_goal_price,
+            "fund_total_price" : d.fund_total_price,
+            "percent" : int(d.fund_total_price / d.fund_goal_price * 100)
+
+        })
+
+
+    
 
     return render(
         request,
         'fund_view/main.html',
         {
-            "data" : data,
-            "img" : img
+            "data" : result,
+
         }
     )
