96435d0a1af98dd85ecf6c8f18c2d49ba6a61298
sejoe-dct <joe240599@gmail.com>
2022-01-23 18:57:41 +0900

    update : fund_detail

--- fundingapp/views.py
+++ fundingapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import FundingBoard, User1, JoinFund
+from django.http import HttpResponse
 
 def select(request):
     data = FundingBoard.objects.all()
-
 
     img = []
     for d in data:
@@ -12,6 +12,22 @@ def select(request):
     return render(
         request,
         'fund_view/main.html',
+        {
+            "data" : data,
+            "img" : img
+        }
+    )
+
+def detail(request):
+    data = FundingBoard.objects.filter(board_id=1)
+
+    img = []
+    for d in data:
+        img.append("/static/" + str(d.board_id) + "jpg")
+
+    return render(
+        request,
+        'fund_view/fund_detail.html',
         {
             "data" : data,
             "img" : img
