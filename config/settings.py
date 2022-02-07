c5b2979296afff2009239c09da36fc87ed3a91ac
Jongwon KIM <46054315+JONWON2@users.noreply.github.com>
2022-02-07 15:14:33 +0900

    DB 내용 수정

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.35','*']
+ALLOWED_HOSTS = ['192.168.0.35','*' ,'0.0.0.0']
 
 
 # Application definition
@@ -85,29 +85,28 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    ## AIVLE DB 입니다.!
+#     ## AIVLE DB 입니다.!
     
-    'custom': { # thirdapp에서 사용할 데이터베이스 설정 추가
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'busan04',
-    'USER': 'busan04',
-    'PASSWORD': 'busan04',
-    'HOST': '13.125.52.234',
-    'PORT': 3306
+#     'custom': { # thirdapp에서 사용할 데이터베이스 설정 추가
+#     'ENGINE': 'django.db.backends.mysql',
+#     'NAME': 'busan04',
+#     'USER': 'busan04',
+#     'PASSWORD': 'busan04',
+#     'HOST': '13.125.52.234',
+#     'PORT': 3306
 
-    ##
+#     ##
 
     ## 저희 DB 살릴 때 사용하시면 됩니다.
 
-    # 'custom': { # thirdapp에서 사용할 데이터베이스 설정 추가
-    # 'ENGINE': 'django.db.backends.mysql',
-    # 'NAME': 'ktaivle',
-    # 'USER': 'kt_aivle',
-    # 'PASSWORD': 'aivle',
-    # 'HOST': '203.250.72.99',
-    # 'PORT': 8000
+    'custom': { # thirdapp에서 사용할 데이터베이스 설정 추가
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'ktaivle',
+    'USER': 'kt_aivle',
+    'PASSWORD': 'aivle',
+    'HOST': '203.250.72.99',
+    'PORT': 8000
 
-    ##
     }   
     
 }
